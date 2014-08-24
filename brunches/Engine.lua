336b8005518201a35b0cf5cfa1e6c81f95e47cf4
tanitta <ttata.trit@gmail.com>
2014-08-24 18:57:19 +0900

    Change torque balance 0.7:1.3 -> 0.8:1.2

--- brunches/Engine.lua
+++ brunches/Engine.lua
@@ -14,32 +14,32 @@ namespace"sorceress"{
 			self.TCS_RR = trit_Component.TCS_typeR:new(0.6)
 			self.numOriginPower = 70000;
 		end);
-		
+
 		method"initTCS"
 		:body(function(self)
-			
+
 		end);
-			
+
 		method "SetSensors"
 		:body(function(self,sensors)
 			self.sensors = sensors;
 		end);
-		
+
 		method"SetInputs"
 		:body(function(self,inputs)
 			self.inputs = inputs;
 		end);
-		
+
 		method"SetPerHandle"
 		:body(function(self,per)
 			self.inputs.perHandle = per;
 		end);
-		
+
 		method"SetModes"
 		:body(function(self,modes)
 			self.modes = modes;
 		end);
-		
+
 		method"GetVal"
 		:body(function(self,str)
 			return self.val[str]
@@ -50,18 +50,18 @@ namespace"sorceress"{
 			self.TCS_FR:SetInput(self.sensors.frRoot.lvz, -self.sensors.fr.nwy)
 			self.TCS_RL:SetInput(-self.sensors.rlRoot.lvz, self.sensors.rl.nwy)
 			self.TCS_RR:SetInput(-self.sensors.rrRoot.lvz, -self.sensors.rr.nwy)
-			
+
 			self.TCS_FL()
 			self.TCS_FR()
 			self.TCS_RL()
 			self.TCS_RR()
-			
+
 			local powMain=self.inputs.accel*(7.337*math.abs(self.sensors.core.lvz)^2 + 508.7*math.abs(self.sensors.core.lvz) ) + (1654 +self.numOriginPower*2)*self.inputs.accel
 
-			self.val.fl=powMain*(1-self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_FL:GetOutput()*0.7*(1-self.modes.flight)
-			self.val.fr=powMain*(1+self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_FR:GetOutput()*0.7*(1-self.modes.flight)
-			self.val.rl=powMain*(1-self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_RL:GetOutput()*1.3*(1-self.modes.flight)
-			self.val.rr=powMain*(1+self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_RR:GetOutput()*1.3*(1-self.modes.flight)
+			self.val.fl=powMain*(1-self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_FL:GetOutput()*0.8*(1-self.modes.flight)
+			self.val.fr=powMain*(1+self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_FR:GetOutput()*0.8*(1-self.modes.flight)
+			self.val.rl=powMain*(1-self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_RL:GetOutput()*1.2*(1-self.modes.flight)
+			self.val.rr=powMain*(1+self.inputs.perHandle*(0.3+self.inputs.deff*0.7))*self.TCS_RR:GetOutput()*1.2*(1-self.modes.flight)
 		end);
 	};
 };
